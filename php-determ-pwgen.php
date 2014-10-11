d1e1e37add3101bc48334057fb6ac0988e645fe3
Jakob H. Renpening <jakobhans@gmail.com>
2014-10-11 10:54:28 -0500

    First lines for script file. Looking for silent prompt input in PHP for the seed

---
+++ php-determ-pwgen.php
@@ -0,0 +1,7 @@
+<?php
+
+fwrite(STDOUT, "php-determ-pwgen. A deterministic password generator in PHP");
+fwrite(STDOUT, "By jakobhans (https://github.com/jakobhans/php-determ-pwgen). Version 1.0");
+fwrite(STDOUT, "Inspired by determ-pwgen by I3ck (https://github.com/I3ck/determ-pwgen). Same logic is used, no code has been reused");
+
+?>
