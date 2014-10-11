1d1952c6d3f53abc70b8db17a33f8fceb5f92d47
Jakob H. Renpening <jakobhans@gmail.com>
2014-10-11 10:56:24 -0500

    Added linebreaks :P

--- php-determ-pwgen.php
+++ php-determ-pwgen.php
@@ -1,7 +1,8 @@
 <?php
 
-fwrite(STDOUT, "php-determ-pwgen. A deterministic password generator in PHP");
-fwrite(STDOUT, "By jakobhans (https://github.com/jakobhans/php-determ-pwgen). Version 1.0");
-fwrite(STDOUT, "Inspired by determ-pwgen by I3ck (https://github.com/I3ck/determ-pwgen). Same logic is used, no code has been reused");
+fwrite(STDOUT, "php-determ-pwgen\n");
+fwrite(STDOUT, "A deterministic password generator in PHP\n");
+fwrite(STDOUT, "By jakobhans (https://github.com/jakobhans/php-determ-pwgen). Version 1.0\n");
+fwrite(STDOUT, "Inspired by determ-pwgen by I3ck (https://github.com/I3ck/determ-pwgen). Same logic is used, no code has been reused\n");
 
 ?>
