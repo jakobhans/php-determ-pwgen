12ac77abc866f2a3973fa464c5a538d2dfcc6b9e
Jakob H. Renpening <jakobhans@gmail.com>
2014-12-16 20:58:38 -0600

    Working on class

--- determPwGenClass.php
+++ determPwGenClass.php
@@ -2,16 +2,27 @@
 
 class determPwGen
 {
-
     private $rounds;
+    private $salt;
     private $site_label;
     private $username;
 
-    function __construct($rounds, $site_label, $username)
+    function __construct($rounds = 1000, $salt, $site_label, $username)
     {
         $this->rounds = $rounds;
+        $this->salt = $salt;
         $this->site_label = $site_label;
         $this->username = $username;
     } 
 
+    function generateHash() {
+        if (isset($this->salt) && isset($this->site_label) && isset($this->username)) {
+            $i = 0;
+            $string = $this->salt . $this->site_label . $this->username;
+            $hash = '';
+            while($i <= $this->rounds) {
+                // Hash string
+            }
+        }
+    }
 }
--- php-determ-pwgen.php
+++ php-determ-pwgen.php
@@ -3,6 +3,6 @@
 fwrite(STDOUT, "php-determ-pwgen\n");
 fwrite(STDOUT, "A deterministic password generator in PHP\n");
 fwrite(STDOUT, "By jakobhans (https://github.com/jakobhans/php-determ-pwgen). Version 1.0\n");
-fwrite(STDOUT, "Inspired by determ-pwgen by I3ck (https://github.com/I3ck/determ-pwgen). Same logic is used, no code has been reused\n");
+fwrite(STDOUT, "Inspired by determ-pwgen by I3ck (https://github.com/I3ck/determ-pwgen). Same logic is used, no code has been reused.\n");
 
 ?>
